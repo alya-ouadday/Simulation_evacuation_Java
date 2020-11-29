d8d38d4c832bcf6f5c48a132fd4544dcdc23a1f0
Alya BEN OUADDAY <63308468+alya-ouadday@users.noreply.github.com>
2020-11-29 11:23:20 +0100

    Add files via upload

---
+++ Projet/src/proj/simuagent/Strategy.java
@@ -0,0 +1,18 @@
+package proj.simuagent;
+import proj.physics.Personne;
+import proj.physics.Vecteur2D;
+/**
+ *<ul> L'interface Strategy représente le "contrat" pour la création des classes concrètes l'implémentant .</ul>
+ *Elle impose la redéfinition d'une méthode deplacement(Personne p).
+ * @author BEN OUADDAY Alya 
+ * @author LAUJIN Laurine
+ *
+ */
+public interface Strategy {
+	/**
+	 * Cette méthdode indique comment la personnne va se deplacer.
+	 * @param p La personne qui va se déplacer.
+	 * @return la cible vers laquelle va se diriger la personne
+	 */
+	public Vecteur2D deplacement(Personne p);
+}
